f06916d027a199d29b92b46c555cd9c05f17845c
Lakshay0786 <lakshay3899@gmail.com>
2022-09-21 17:48:54 +0530

    done

--- src/components/dataSlider.js
+++ src/components/dataSlider.js
@@ -3,28 +3,53 @@ import { v4 as uuidv4 } from "uuid";
 const dataSlider = [
   {
     id: uuidv4(),
-    title: "Project 1",
-    subTitle: "Lorem"
+    title: "Adventure ",
+    subTitle: "My first project using React.js & css-module  skills. Learned how to build a responsive hamburger menu, padding, margin, typography, deploying on Netlify",
+    img: 'localhost_3000_ (2).png',
+
+    stack1: 'JavaScript',
+    stack2: 'tailwind CSS',
+    stack3: 'React.js',
+    stack4: 'Netlify'
   },
   {
     id: uuidv4(),
-    title: "Project 2",
-    subTitle: "Lorem"
+    title: "Netflix Clone",
+    subTitle: "A functional & responsive Netflix clone built using react.js, SCSS, Fetching data from an Api. Key features include getting data from the server ",
+    img: 'Screenshot from 2022-09-21 11-25-25.png',
+    stack1: 'JavaScript',
+    stack2: 'Axios',
+    stack3: 'React.js',
+    stack4: 'Netlify'
   },
   {
     id: uuidv4(),
-    title: "Project 3",
-    subTitle: "Lorem"
+    title: "Space Tourism ",
+    subTitle: "Space Tourism landing page is a frontend practice challenge to sharpen up my Tailwind CSS skills. Key features includes Nesting of react-router-dom, fetch data dynamically, alternating sections.",
+    img: 'Screenshot from 2022-09-21 11-29-09.png',
+
+    stack1: 'JavaScript',
+    stack2: 'tailwind CSS',
+    stack3: 'React.js',
+    stack4: 'Netlify'
   },
   {
     id: uuidv4(),
-    title: "Project 4",
-    subTitle: "Lorem"
+    title: "Parallax scrolling ",
+    subTitle: "In this  background images move past the camera more slowly than foreground images, creating an illusion of depth in a 2D scene of distance.",
+    img: 'Screenshot from 2022-09-21 11-30-36.png',
+    stack1: 'JavaScript',
+    stack2: 'CSS',
   },
   {
     id: uuidv4(),
-    title: "Project 5",
-    subTitle: "Lorem"
+    title: "Discord Website Clone",
+    subTitle: "Discord landing page is a frontend practice challenge to sharpen up my  CSS skills. learned about Positions and how to style a layout",
+    img: 'Screenshot from 2022-09-21 11-39-35.png',
+
+    stack1: 'Html',
+    stack2: 'CSS',
+    stack3: 'Git',
   },
 ];
 
--- src/components/swiper.jsx
+++ src/components/swiper.jsx
@@ -15,12 +15,12 @@ function SkillSwiper() {
     return (
         <div className="swiper-body">
             <h1 className="swiper-h1">Some Things I've Built. </h1>
-            <p>I'm a great believer in learning by building, so I build stuff from scratch, get stuck, google & repeat because everything is figureoutable.</p>
+            <p className="projects-p">I'm a great believer in learning by building, so I build stuff from scratch, get stuck, google & repeat because everything is figureoutable.</p>
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 2500,
+                    delay: 4000,
                     disableOnInteraction: false,
                 }}
                 pagination={{
@@ -33,9 +33,21 @@ function SkillSwiper() {
                 {dataSlider.map((obj) => {
                     return (
                         <SwiperSlide>
-
-                            <h2>{obj.title}</h2>
-
+                            <div className="projects-top">
+                                <div className="img-projects-div">
+                                    <img className="project-img" src={require(`../project-img/best/${obj.img}`)} alt='photo2' />
+                                </div>
+                                <div className="text-projects">
+                                    <h2>{obj.title}</h2>
+                                    <h4>{obj.subTitle}</h4>
+                                    <div className="stack">
+                                        <h4>{obj.stack1}</h4>
+                                        <h4>{obj.stack2}</h4>
+                                        <h4>{obj.stack3}</h4>
+                                        <h4>{obj.stack4}</h4>
+                                    </div>
+                                </div>
+                            </div>
                         </SwiperSlide>
                     )
                 })}
